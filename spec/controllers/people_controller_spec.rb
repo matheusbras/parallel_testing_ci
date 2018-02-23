fda6dcb1e2e17a0cce3bc8e966406b6a8812f2b9
Matheus Bras <bras.matheus@gmail.com>
2018-02-23 17:09:22 -0300

    w

--- spec/controllers/people_controller_spec.rb
+++ spec/controllers/people_controller_spec.rb
@@ -25,116 +25,118 @@ require 'rails_helper'
 
 RSpec.describe PeopleController, type: :controller do
 
-  # This should return the minimal set of attributes required to create a valid
-  # Person. As you add validations to Person, be sure to
-  # adjust the attributes here as well.
-  let(:valid_attributes) {
-    { name: 'Nome' }
-  }
+  100.times do
+    # This should return the minimal set of attributes required to create a valid
+    # Person. As you add validations to Person, be sure to
+    # adjust the attributes here as well.
+    let(:valid_attributes) {
+      { name: 'Nome' }
+    }
 
-  let(:invalid_attributes) {
-    { name: '' }
-  }
+    let(:invalid_attributes) {
+      { name: '' }
+    }
 
-  # This should return the minimal set of values that should be in the session
-  # in order to pass any filters (e.g. authentication) defined in
-  # PeopleController. Be sure to keep this updated too.
-  let(:valid_session) { {} }
+    # This should return the minimal set of values that should be in the session
+    # in order to pass any filters (e.g. authentication) defined in
+    # PeopleController. Be sure to keep this updated too.
+    let(:valid_session) { {} }
 
-  describe "GET #index" do
-    it "returns a success response" do
-      person = Person.create! valid_attributes
-      get :index, params: {}, session: valid_session
-      expect(response).to be_success
+    describe "GET #index" do
+      it "returns a success response" do
+        person = Person.create! valid_attributes
+        get :index, params: {}, session: valid_session
+        expect(response).to be_success
+      end
     end
-  end
 
-  describe "GET #show" do
-    it "returns a success response" do
-      person = Person.create! valid_attributes
-      get :show, params: {id: person.to_param}, session: valid_session
-      expect(response).to be_success
+    describe "GET #show" do
+      it "returns a success response" do
+        person = Person.create! valid_attributes
+        get :show, params: {id: person.to_param}, session: valid_session
+        expect(response).to be_success
+      end
     end
-  end
 
-  describe "GET #new" do
-    it "returns a success response" do
-      get :new, params: {}, session: valid_session
-      expect(response).to be_success
+    describe "GET #new" do
+      it "returns a success response" do
+        get :new, params: {}, session: valid_session
+        expect(response).to be_success
+      end
     end
-  end
 
-  describe "GET #edit" do
-    it "returns a success response" do
-      person = Person.create! valid_attributes
-      get :edit, params: {id: person.to_param}, session: valid_session
-      expect(response).to be_success
+    describe "GET #edit" do
+      it "returns a success response" do
+        person = Person.create! valid_attributes
+        get :edit, params: {id: person.to_param}, session: valid_session
+        expect(response).to be_success
+      end
     end
-  end
 
-  describe "POST #create" do
-    context "with valid params" do
-      it "creates a new Person" do
-        expect {
+    describe "POST #create" do
+      context "with valid params" do
+        it "creates a new Person" do
+          expect {
+            post :create, params: {person: valid_attributes}, session: valid_session
+          }.to change(Person, :count).by(1)
+        end
+
+        it "redirects to the created person" do
           post :create, params: {person: valid_attributes}, session: valid_session
-        }.to change(Person, :count).by(1)
+          expect(response).to redirect_to(Person.last)
+        end
       end
 
-      it "redirects to the created person" do
-        post :create, params: {person: valid_attributes}, session: valid_session
-        expect(response).to redirect_to(Person.last)
+      context "with invalid params" do
+        it "returns a success response (i.e. to display the 'new' template)" do
+          post :create, params: {person: invalid_attributes}, session: valid_session
+          expect(response).to be_success
+        end
+      end
+    end
+
+    describe "PUT #update" do
+      context "with valid params" do
+        let(:new_attributes) {
+          { name: 'Nome 2' }
+        }
+
+        it "updates the requested person" do
+          person = Person.create! valid_attributes
+          put :update, params: {id: person.to_param, person: new_attributes}, session: valid_session
+          person.reload
+          expect(person.name).to eq('Nome 2')
+        end
+
+        it "redirects to the person" do
+          person = Person.create! valid_attributes
+          put :update, params: {id: person.to_param, person: valid_attributes}, session: valid_session
+          expect(response).to redirect_to(person)
+        end
+      end
+
+      context "with invalid params" do
+        it "returns a success response (i.e. to display the 'edit' template)" do
+          person = Person.create! valid_attributes
+          put :update, params: {id: person.to_param, person: invalid_attributes}, session: valid_session
+          expect(response).to be_success
+        end
       end
     end
 
-    context "with invalid params" do
-      it "returns a success response (i.e. to display the 'new' template)" do
-        post :create, params: {person: invalid_attributes}, session: valid_session
-        expect(response).to be_success
-      end
-    end
-  end
-
-  describe "PUT #update" do
-    context "with valid params" do
-      let(:new_attributes) {
-        { name: 'Nome 2' }
-      }
-
-      it "updates the requested person" do
+    describe "DELETE #destroy" do
+      it "destroys the requested person" do
         person = Person.create! valid_attributes
-        put :update, params: {id: person.to_param, person: new_attributes}, session: valid_session
-        person.reload
-        expect(person.name).to eq('Nome 2')
+        expect {
+          delete :destroy, params: {id: person.to_param}, session: valid_session
+        }.to change(Person, :count).by(-1)
       end
 
-      it "redirects to the person" do
+      it "redirects to the people list" do
         person = Person.create! valid_attributes
-        put :update, params: {id: person.to_param, person: valid_attributes}, session: valid_session
-        expect(response).to redirect_to(person)
-      end
-    end
-
-    context "with invalid params" do
-      it "returns a success response (i.e. to display the 'edit' template)" do
-        person = Person.create! valid_attributes
-        put :update, params: {id: person.to_param, person: invalid_attributes}, session: valid_session
-        expect(response).to be_success
-      end
-    end
-  end
-
-  describe "DELETE #destroy" do
-    it "destroys the requested person" do
-      person = Person.create! valid_attributes
-      expect {
         delete :destroy, params: {id: person.to_param}, session: valid_session
-      }.to change(Person, :count).by(-1)
-    end
-
-    it "redirects to the people list" do
-      person = Person.create! valid_attributes
-      delete :destroy, params: {id: person.to_param}, session: valid_session
-      expect(response).to redirect_to(people_url)
+        expect(response).to redirect_to(people_url)
+      end
     end
   end
 end
