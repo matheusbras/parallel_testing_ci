91f717b9c03641cdeb3ebd762fc942004c6c502c
Matheus Bras <bras.matheus@gmail.com>
2018-02-23 17:19:08 -0300

    asda

--- spec/models/person_spec.rb
+++ spec/models/person_spec.rb
@@ -1,5 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Person, type: :model do
-  it { is_expected.to validate_presence_of(:name) }
+  1000.times do
+    it { is_expected.to validate_presence_of(:name) }
+  end
 end
